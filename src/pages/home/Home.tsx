f9e811f21ede7f5bff8c4ae6c7eabb6d7fef5932
정석우 <ingal777@naver.com>
2022-03-16 13:23:27 +0900

    Design main page 수정

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,21 +4,33 @@ import './Home.scss';
 
 function Home() {
   return (
-    <div className="mainBackground">
-      <div className="mainHeaderText">Delgo!</div>
-      <div className="mainPlaceImage">
+    <>
+    <div className="main-background">
+      <div className="main-headertext">Delgo!</div>
+      <div className="main-placeimage">
         <img src={`${process.env.PUBLIC_URL}/assets/images/mainPlaceImage.png`} alt="" />
-        <div className="mainPlaceImageSubText">바다가 보이는 여름숙소</div>
-        <div className="mainPlaceImageName">숙초 코코네집</div>
+        <div className="main-placeimage-subtext">바다가 보이는 여름숙소</div>
+        <div className="main-placeimage-name">숙초 코코네집</div>
       </div>
-      <div className="recommendedPlaceText">델고갈만한 숙소</div>
-      <div className="recommendedPlaceImage">
+      <div className="recommendedplace-text">델고갈만한 숙소</div>
+      <div className="recommendedplace-image">
         <img src={`${process.env.PUBLIC_URL}/assets/images/recommendedPlaceImage.png`} alt="" />
-        <div className="recommendedPlaceName">멍멍이네 하우스</div>
-        <div className="recommendedPlaceLocation">강원도 속초시 조앙동</div>
+        <div className="recommendedplace-name">멍멍이네 하우스</div>
+        <div className="recommendedplace-location">강원도 속초시 조앙동</div>
       </div>
-      <Footer />
+      <div className="recommendedplace-image">
+        <img src={`${process.env.PUBLIC_URL}/assets/images/recommendedPlaceImage.png`} alt="" />
+        <div className="recommendedplace-name">멍멍이네 하우스</div>
+        <div className="recommendedplace-location">강원도 속초시 조앙동</div>
+      </div>
+      <div className="recommendedplace-image">
+        <img src={`${process.env.PUBLIC_URL}/assets/images/recommendedPlaceImage.png`} alt="" />
+        <div className="recommendedplace-name">멍멍이네 하우스</div>
+        <div className="recommendedplace-location">강원도 속초시 조앙동</div>
+      </div>
     </div>
+    <Footer />
+    </>
   );
 }
 
