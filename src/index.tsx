8d60fb79958d9abb8c04e11be5cca8cc9a50b3bc
정석우 <ingal777@naver.com>
2022-03-17 19:46:09 +0900

    ADD editor node page ui + design home page + Rename common folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
 
-let persistor = persistStore(store);
+
+ const persistor = persistStore(store);
 
 
 
--- src/pages/home/EditorNote.tsx
+++ src/pages/home/EditorNote.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import "./EditorNote.scss";
 
 function EditorNote() {
   return (
-    <div>EditorNote</div>
-  )
+    <div className="editor-background">
+      <div className="editor-subtext">바다가 보이는 여름숙소</div>
+      <div className="editor-headertext">속초 코코네집</div>
+      <div className="editor-place">
+        <img src={`${process.env.PUBLIC_URL}/assets/images/editorImage.png`} alt="" />
+        <div className="editor-place-description">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim
+          velit mollit. Exercitation veniam consequat sunt nostrud amet.
+        </div>
+      </div>
+      <div className="editor-place">
+        <img src={`${process.env.PUBLIC_URL}/assets/images/editorImage.png`} alt="" />
+        <div className="editor-place-description">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim
+          velit mollit. Exercitation veniam consequat sunt nostrud amet.
+        </div>
+      </div>
+      <div className="editor-place">
+        <img src={`${process.env.PUBLIC_URL}/assets/images/editorImage.png`} alt="" />
+        <div className="editor-place-description">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim
+          velit mollit. Exercitation veniam consequat sunt nostrud amet.
+        </div>
+      </div>
+      <div className="editor-footer">
+          <div className="laern-more">자세히 보러가기</div>
+      </div>
+    </div>
+  );
 }
 
 export default EditorNote
