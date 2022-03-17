4aeda4c59ab325d4376ec401239c5d8763e1300b
정석우 <ingal777@naver.com>
2022-03-17 20:15:08 +0900

    Docs class명 변경

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,13 +6,13 @@ function Home() {
   return (
     <>
       <div className="home-background">
-        <div className="main-headertext">Delgo!</div>
+        <div className="main-header-text">Delgo!</div>
         <div className="main-place">
           <img src={`${process.env.PUBLIC_URL}/assets/images/mainPlaceImage.png`} alt="" />
           <div className="main-place-subtext">바다가 보이는 여름숙소</div>
           <div className="main-place-name">숙초 코코네집</div>
         </div>
-        <div className="recommended-placetext">델고갈만한 숙소</div>
+        <div className="recommended-place-text">델고갈만한 숙소</div>
         <div className="recommended-place">
           <img src={`${process.env.PUBLIC_URL}/assets/images/recommendedPlaceImage.png`} alt="" />
           <div className="recommended-place-name">멍멍이네 하우스</div>
