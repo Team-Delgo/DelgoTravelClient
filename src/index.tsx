6f7b0a2c048fc5916bb5dda5269b9e2e285afc6b
hoin <72450781+lucid-jin@users.noreply.github.com>
2022-03-19 12:19:58 +0900

    Add: Constants folder(path, env, regex)

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore } from 'redux-persist';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistStore} from 'redux-persist';
 import App from './App';
 import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
@@ -11,17 +11,15 @@ import reportWebVitals from './reportWebVitals';
 const persistor = persistStore(store);
 
 
-
-
 ReactDOM.render(
   <BrowserRouter>
-  <React.StrictMode>
-  <Provider store={store}>
-  <PersistGate loading={null} persistor={persistor}>
-    <App />
-    </PersistGate>
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App/>
+        </PersistGate>
+      </Provider>
+    </React.StrictMode>
   </BrowserRouter>
   ,
   document.getElementById('root')
