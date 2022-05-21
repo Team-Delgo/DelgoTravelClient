f4223b085bcb4752711ac71ea2386370480ea087
정석우 <ingal777@naver.com>
2022-05-21 21:27:51 +0900

    Fix heart 클릭시 파란하이라이트

--- src/common/components/Heart.jsx
+++ src/common/components/Heart.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Heart from "react-animated-heart"
 
-function HeartComponent({wishList,handleWishList}) {
+
+function HeartComponent({ wishList, handleWishList }) {
   return (
-    <Heart isClick={wishList} onClick={handleWishList} />
-  )
+      <Heart className="heart" isClick={wishList} onClick={handleWishList} />
+  );
 }
 
 export default HeartComponent
