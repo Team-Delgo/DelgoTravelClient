b6735af5fad3e9696099347901d562ce5b50adca
cksrb63 <cksr1@naver.com>
2022-07-05 17:47:07 +0900

    add main reservation scroll

--- src/pages/home/HomeReservation.tsx
+++ src/pages/home/HomeReservation.tsx
@@ -1,27 +1,85 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import './HomeReservation.scss';
 
 function HomeReservation() {
   const scrollRef = useRef<any>(null);
+  const [position, setPosition] = useState(0);
+  const [startPosition, setStartPosition] = useState(0);
+  const [page, setPage] = useState(0);
+  const [isScrolling, setIsScrolling] = useState(false);
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   setIsScrolling(false);
+  //   setTimeout(() => {
+  //     if (!isScrolling) {
+  //       console.log(1);
+  //     }
+  //   }, 50)
+  // }, [isScrolling]);
+
+  const scrollHandler = () => {
+    setIsScrolling(true);
+    setPosition(scrollRef.current.scrollLeft);
+  };
+
+  const mouseLeaveHandler = () => {
     const el = scrollRef.current;
-    if (el) {
-      const onWheel = (e: any) => {
-        if (e.deltaY === 0) return;
-        e.preventDefault();
+    const width = el.offsetWidth;
+    console.log(position, startPosition);
+    setTimeout(() => {
+      if (position < startPosition) {
         el.scrollTo({
-          left: el.scrollLeft + e.deltaY,
+          left: (Math.floor(el.scrollLeft / width)) * width,
           behavior: "smooth"
         });
-      };
-      el.addEventListener("wheel", onWheel);
-      return () => el.removeEventListener("wheel", onWheel);
-    }
-  }, []);
+        setPosition(startPosition - width);
+        setPage(Math.floor(el.scrollLeft / width));
+      }
+      else if (position > startPosition) {
+        el.scrollTo({
+          left: (Math.floor(el.scrollLeft / width) + 1) * width,
+          behavior: "smooth"
+        });
+        setPosition(startPosition + width);
+        setPage(Math.floor(el.scrollLeft / width));
+      } else {
+        el.scrollTo({
+          left: startPosition,
+          behavior: "smooth"
+        })
+      }
+    }, 400);
+  };
+
+
+
+  const mouseDownHandler = () => {
+    const el = scrollRef.current;
+    const width = el.offsetWidth;
+    const currentPosition = el.scrollLeft;
+    const main = (Math.floor(currentPosition / width)) * width;
+    setStartPosition(main);
+  };
+
+  // useEffect(() => {
+  //   const el = scrollRef.current;
+  //   const width = el.offsetWidth;
+  //   const currentPosition = el.scrollLeft;
+  //   console.log(currentPosition);
+  //   setTimeout(() => {
+  //     el.scrollTo({
+  //       left: (Math.floor(currentPosition / width)) * width,
+  //       behavior: "smooth"
+  //     });
+  //     console.log(currentPosition);
+  //   }, 500);
+
+  // }, [endPosition]);
+
+  console.log(page);
 
   return (
-    <div className="homemodal" ref={scrollRef} >
+    <div aria-hidden="true" className="homemodal" ref={scrollRef} onScroll={scrollHandler} onTouchStart={mouseDownHandler} onTouchEnd={mouseLeaveHandler}>
       <div className="homemodal-item">
         <div className="homemodal-item-card">1</div>
       </div>
