ea93f8bb2302f3006286a94aafb8ba0e260b7dab
jungseokwoo <wjdtjrdn6@gmail.com>
2022-07-24 11:20:10 +0900

    ADD:review writing form

--- src/pages/reservation/reservationConfirmPage/modal/ReservationCancleModal.jsx
+++ src/pages/reservation/reservationConfirmPage/modal/ReservationCancleModal.jsx
@@ -27,7 +27,7 @@ function ReservationCancleModal({ reservationCancleModal, closeReservationCancle
   }
 
   return (
-    <Sheet isOpen={reservationCancleModal} onClose={closeReservationCancleModal} snapPoints={[500, 500, 100, 0]}>
+    <Sheet isOpen={reservationCancleModal} onClose={closeReservationCancleModal} snapPoints={[410, 410, 100, 0]}>
       <Sheet.Container>
         <Sheet.Header />
         <Sheet.Content>
@@ -52,21 +52,6 @@ function ReservationCancleModal({ reservationCancleModal, closeReservationCancle
       </Sheet.Container>
       <Sheet.Backdrop />
     </Sheet>
-    // <Modal style={regionSelectionModalStyle} isOpen={reservationCancleModal} onRequestClose={closeReservationCancleModal} ariaHideApp={false}>
-    //   <div className="reservation-cancle-confirm">
-    //     <h3 className="reservation-cancle-confirm-label">예약을 취소하시겠어요?</h3>
-    //     <div className="reservation-cancle-place-info">
-    //       <header className="reservation-cancle-place-info-name">밸런스독</header>
-    //       <div className="reservation-cancle-place-info-address">경기도 양평시 지평균 바깥균 1길 32-19</div>
-    //       <div className="reservation-cancle-place-info-room-type">온수풀,더블베드타입</div>
-    //       <div className="reservation-cancle-place-info-date">22.05.06(월) - 22.05.07(화)</div>
-    //     </div>
-    //     <div className="reservation-cancle-button-container">
-    //       <button type='button' className="reservation-cancle-button" onClick={cancleReservation}>네</button>
-    //       <button type='button' className="reservation-cancle-button" onClick={closeReservationCancleModal}>아니요</button>
-    //     </div>
-    //   </div>
-    // </Modal>
   );
 }
 export default ReservationCancleModal;
