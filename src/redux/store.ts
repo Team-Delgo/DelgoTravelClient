4b476774596aea751b0fb39138ee608e175258d0
jungseokwoo <wjdtjrdn6@gmail.com>
2022-07-24 14:33:22 +0900

    MODIFY:redux-devtools production

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -32,7 +32,8 @@ const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
   reducer: { persist: persistedReducer, token: tokenSlice, error: errorSlice, date: dateSlice},
-  devTools: process.env.NODE_ENV !== 'production',
+  // devTools: process.env.NODE_ENV !== 'production',
+  devTools: true,
 });
 
 export default store;
