6f067635762f21a3340bdb9c1702a06886b88a49
cksrb63 <cksr1@naver.com>
2022-08-02 17:00:20 +0900

    ADD KAKAO login

--- src/constants/url.cosnt.ts
+++ src/constants/url.cosnt.ts
@@ -11,25 +11,21 @@ export const url = 'http://61.97.186.174:8080/';
 //    CALL_BACK_URL = 'http://localhost:3000'
 // }
 export const KAKAO = {
-    REST_API_KEY : "ed29d568272e3eb51b493659e2d6e9eb",
-    CALL_BACK_URL : `${process.env.REACT_APP_BASE_URL}/oauth/callback/kakao`,
-    KAKAO_AUTH_URL : `https://kauth.kakao.com/oauth/authorize?client_id=ed29d568272e3eb51b493659e2d6e9eb&redirect_uri=${process.env.REACT_APP_BASE_URL}/oauth/callback/kakao&response_type=code`
-}
+  // REST_API_KEY : "ed29d568272e3eb51b493659e2d6e9eb",
+  REST_API_KEY: 'b40f84b68ce44634317bb5530b0166c1',
+  CALL_BACK_URL: `${process.env.REACT_APP_BASE_URL}/oauth/callback/kakao`,
+  KAKAO_AUTH_URL: `https://kauth.kakao.com/oauth/authorize?client_id=b40f84b68ce44634317bb5530b0166c1&redirect_uri=${process.env.REACT_APP_BASE_URL}/oauth/callback/kakao&response_type=code`,
+};
 
 export const NAVER = {
-    CLIENT_ID : "AsoX91fTgAHzXvMW9Mik",
-    CLIENT_SECRET : "sLk3D0NqzW",
-    STATE_STRING : "9kgsGTfH4j7IyAkg",
-    CALL_BACK_URL : `${process.env.REACT_APP_BASE_URL}/oauth/callback/naver`,
-    NAVER_AUTH_URL : `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=AsoX91fTgAHzXvMW9Mik&state=9kgsGTfH4j7IyAkg&redirect_uri=${process.env.REACT_APP_BASE_URL}/oauth/callback/naver`
-}
+  CLIENT_ID: 'AsoX91fTgAHzXvMW9Mik',
+  CLIENT_SECRET: 'sLk3D0NqzW',
+  STATE_STRING: '9kgsGTfH4j7IyAkg',
+  CALL_BACK_URL: `${process.env.REACT_APP_BASE_URL}/oauth/callback/naver`,
+  NAVER_AUTH_URL: `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=AsoX91fTgAHzXvMW9Mik&state=9kgsGTfH4j7IyAkg&redirect_uri=${process.env.REACT_APP_BASE_URL}/oauth/callback/naver`,
+};
 
 export const TOSS = {
-    CLIENT_KEY : 'test_ck_XLkKEypNArWzAYzmpbjVlmeaxYG5',
-    SECRET_KEY : 'test_sk_5mBZ1gQ4YVXzygzAM0a8l2KPoqNb'
-}
-
-
-
-
-
+  CLIENT_KEY: 'test_ck_XLkKEypNArWzAYzmpbjVlmeaxYG5',
+  SECRET_KEY: 'test_sk_5mBZ1gQ4YVXzygzAM0a8l2KPoqNb',
+};
