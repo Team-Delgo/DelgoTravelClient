6264c01a0de5ca1fd096d8378aa997bfa664c52f
ckrb63 <cksr1@naver.com>
2022-09-29 17:01:53 +0900

    ADD NAVER LOGIN LOGIC

--- src/pages/signInPage/socialLogion/NaverRedirectHandler.tsx
+++ src/pages/signInPage/socialLogion/NaverRedirectHandler.tsx
@@ -3,7 +3,9 @@ import axios from 'axios';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import qs from 'qs';
-import { NAVER} from '../../../common/constants/url.cosnt';
+import { NAVER } from '../../../common/constants/url.cosnt';
+import { userActions } from '../../../redux/slice/userSlice';
+import { ROOT_PATH, SIGN_UP_PATH } from '../../../common/constants/path.const';
 
 declare global {
   interface Window {
@@ -15,10 +17,10 @@ declare global {
 function NaverRedirectHandler() {
   const dispatch = useDispatch();
   const code = new URL(window.location.href).searchParams.get("code"); // 네이버 로그인 인증에 성공하면 반환받는 인증 코드, 접근 토큰(access token) 발급에 사용
-  const state = new URL(window.location.href).searchParams.get("state"); 
+  const state = new URL(window.location.href).searchParams.get("state");
   const navigate = useNavigate();
 
-  console.log(code,state)
+  console.log(code, state)
 
   useEffect(() => {
     getToken();
@@ -26,13 +28,13 @@ function NaverRedirectHandler() {
 
   const getToken = async () => {
 
-  //   const userData = await axios.get('https://openapi.naver.com/v1/nid/me', { // 유저정보 api -> cors에러
-  //     headers : {
-  //         'Authorization' : `Bearer ${code}`,
-  //     }
-  // })
+    //   const userData = await axios.get('https://openapi.naver.com/v1/nid/me', { // 유저정보 api -> cors에러
+    //     headers : {
+    //         'Authorization' : `Bearer ${code}`,
+    //     }
+    // })
 
-  // console.log(userData)
+    // console.log(userData)
 
     const payload = qs.stringify({
       grant_type: "authorization_code",
@@ -42,34 +44,57 @@ function NaverRedirectHandler() {
       state,
     });
     try {
-      const res = await axios.post( // 토큰발급 -> 서버에서 해줘야됨
+      const response = await axios.post( // 토큰발급 -> 서버에서 해줘야됨
         "https://nid.naver.com/oauth2.0/token",
-         payload
+        payload
       );
-      console.log(res)
+      console.log(response);
+      const { code, data } = response.data;
+      if (code === 200) {
+        console.log('로그인 성공');
+        dispatch(
+          userActions.signin({
+            isSignIn: true,
+            couponList: data.couponList,
+            user: {
+              id: data.user.userId,
+              nickname: data.user.name,
+              email: data.user.email,
+              phone: data.user.phoneNo,
+              userSocial: data.user.userSocial,
+            },
+            pet: {
+              petId: data.pet.petId,
+              birthday: data.pet.birthday,
+              size: data.pet.size,
+              name: data.pet.name,
+              image: data.user.profile,
+            },
+          }),
+        );
+        const accessToken = response.headers.authorization_access;
+        const refreshToken = response.headers.authorization_refresh;
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('refreshToken', refreshToken);
+        navigate(ROOT_PATH, { replace: true });
+      } else if (code === 370) {
+        console.log('소셜 회원가입');
+        navigate(SIGN_UP_PATH.TERMS, { state: { isSocial: 'K', phone: data.phoneNo, email: data.email } });
+      } else if (code === 380) {
+        console.log('네이버 전화번호 x');
+        navigate(SIGN_UP_PATH.SOCIAL.NO_PHONE, { state: { social: '카카오' } });
+      } else if (code === 381) {
+        console.log('다른 로그인');
+        navigate(SIGN_UP_PATH.SOCIAL.OTHER, { state: { social: data.userSoical, email: data.email } });
+      } else {
+        console.log('네이버 가입 에러');
+      }
     } catch (err) {
       console.log(err);
     }
   };
 
-  const getProfile = async () => {
-    try {
-      window.Naver.API.request({
-        url: 'https://openapi.naver.com/v1/nid/me',  // 유저정보 api -> cors에러
-        success(response:any) {
-            console.log(response);
-        },
-        fail(error:any) {
-            console.log(error);
-        }
-    });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-
-  const moveToPreviousPage = ()=>{
+  const moveToPreviousPage = () => {
     navigate('/user/signin')
   }
 
@@ -77,7 +102,7 @@ function NaverRedirectHandler() {
   return <div>네이버 로그인
     <button type="button" onClick={moveToPreviousPage}>뒤로가기</button>
   </div>
-  
+
 };
 
 export default NaverRedirectHandler
