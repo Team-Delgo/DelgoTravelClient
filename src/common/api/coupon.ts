44b58c49ac8a8b01e86fe3c84e8391387c2cf6e9
정석우 <ingal777@naver.com>
2022-12-20 18:55:01 +0900

    Fix: interceptors error handle

--- src/common/api/coupon.ts
+++ src/common/api/coupon.ts
@@ -4,17 +4,11 @@ import { useErrorHandlers } from './useErrorHandlers';
 
 async function registCoupon(data: { userId: number; couponCode: string },dispatch: any, success: (data: AxiosResponse) => void) {
   try {
-  const accessToken = localStorage.getItem('accessToken') || '';
   const result = await axiosInstance.post(
     `/coupon/regist`,
     {
       userId: data.userId,
       couponCode: data.couponCode,
-    },
-    {
-      headers: {
-        Authorization_Access: accessToken,
-      },
     },
   );
   success(result);
@@ -24,12 +18,7 @@ async function registCoupon(data: { userId: number; couponCode: string },dispatc
 }
 
 async function getCouponList(userId: number) {
-  const accessToken = localStorage.getItem('accessToken') || '';
-  const { data } = await axiosInstance.get(`/coupon/getCouponList?userId=${userId}`, {
-    headers: {
-      Authorization_Access: accessToken,
-    },
-  });
+  const { data } = await axiosInstance.get(`/coupon/getCouponList?userId=${userId}`);
   return data;
 }
 
--- src/common/api/myaccount.ts
+++ src/common/api/myaccount.ts
@@ -3,12 +3,8 @@ import axiosInstance from './interceptors'
 import { useErrorHandlers } from './useErrorHandlers';
 
 async function getMyAccountDataList(userId: number) {
-  const accessToken = localStorage.getItem('accessToken') || '';
   const { data } = await axiosInstance.get(`/myAccount`, {
     params: { userId },
-    headers: {
-      Authorization_Access: accessToken,
-    }
   });
   return data;
 }
@@ -20,17 +16,12 @@ async function changePetInfo(
 ) {
   try {
   const { email, name, birthday, size } = data;
-  const accessToken = localStorage.getItem('accessToken') || '';
   const result = await axiosInstance
     .post(`/changePetInfo`, {
       email,
       name,
       birthday,
       size,
-    },{
-      headers:{
-        Authorization_Access: accessToken
-      }
     })
     success(result)
   } catch (err: any) {
@@ -39,16 +30,11 @@ async function changePetInfo(
 }
 
 async function changePassword(email: string, password: string, dispatch: any,success: (data: AxiosResponse) => void) {
-  const accessToken = localStorage.getItem("accessToken") || '';
   try {
   const result = await axiosInstance
     .post(`/changePassword`, {
       email,
       newPassword: password,
-    },{
-      headers:{
-        Authorization_Access: accessToken,
-      }
     })
     success(result)
   } catch (err: any) {
